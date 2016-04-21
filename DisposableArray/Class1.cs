79b093b8dd0c42eefb135f915f8bf8bbfaf840f1
Tim Bussmann <tim.bussmann@particular.net>
2016-04-21 18:07:08 +0200

    Add project files.

---
+++ DisposableArray/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableArray
+{
+    public class Class1 : IDisposable
+    {
+        private Task[] tasks;
+        private IDisposable[] disposables = null;
+
+        public Class1()
+        {
+            this.tasks = new Task[0];
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
